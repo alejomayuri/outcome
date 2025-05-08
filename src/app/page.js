1829192ff750548dbd076dbb76b5889e8877819e
Alejo Mayuri <alejomayuri@gmail.com>
2025-05-08 12:30:00 -0500

    title y favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -9,53 +10,58 @@ import Recruitment from "./components/Recruitment";
 
 export default function Home() {
   return (
-    <main className="bg-white font-sans text-gray-900 scroll-smooth">
-      {/* Header */}
-      <Header />
+    <>
+      <Head>
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-white font-sans text-gray-900 scroll-smooth">
+        {/* Header */}
+        <Header />
 
-      {/* Hero */}
-      <Hero />
+        {/* Hero */}
+        <Hero />
 
-      {/* Qué ofrecemos */}
-      <div className="md:pt-30">
-        <About/>
-      </div>
-      {/* Servicios */}
-      <Services />
-
-      {/* Pilares */}
-      <Pilares />
-      <div className="bg-black w-full relative pb-50">
-        {/* Quiénes somos */}
-        <WhoWeAre />
-
-        {/* Contacto */}
-        <ContactForm />
-      </div>
-
-      {/* Reclutamiento */}
-      <div className="pt-50">
-        <Recruitment />
-      </div>
-
-      {/* Espacio para el contenido adicional */}
-
-      {/* Footer */}
-      <footer className="bg-purple-700 text-white text-center">
-        <div className="w-full bg-black text-white">
-          <p className="text-md font-bold text-purple-600 py-4">
-            JUNTOS LLEVAMOS LA OPERACIÓN DE TU EMPRESA AL SIGUIENTE NIVEL
-          </p>
-          <div className="flex bg-purple-600 justify-center py-4">
-            <img
-              src="/logo.png"
-              alt="Logo"
-              className="h-12"
-              style={{ filter: 'brightness(0) saturate(100%)' }}
-            />
-          </div>
+        {/* Qué ofrecemos */}
+        <div className="md:pt-30">
+          <About/>
         </div>
-      </footer>
-    </main>
+        {/* Servicios */}
+        <Services />
+
+        {/* Pilares */}
+        <Pilares />
+        <div className="bg-black w-full relative pb-50">
+          {/* Quiénes somos */}
+          <WhoWeAre />
+
+          {/* Contacto */}
+          <ContactForm />
+        </div>
+
+        {/* Reclutamiento */}
+        <div className="pt-50">
+          <Recruitment />
+        </div>
+
+        {/* Espacio para el contenido adicional */}
+
+        {/* Footer */}
+        <footer className="bg-purple-700 text-white text-center">
+          <div className="w-full bg-black text-white">
+            <p className="text-md font-bold text-purple-600 py-4">
+              JUNTOS LLEVAMOS LA OPERACIÓN DE TU EMPRESA AL SIGUIENTE NIVEL
+            </p>
+            <div className="flex bg-purple-600 justify-center py-4">
+              <img
+                src="/logo.png"
+                alt="Logo"
+                className="h-12"
+                style={{ filter: 'brightness(0) saturate(100%)' }}
+              />
+            </div>
+          </div>
+        </footer>
+      </main>
+    </>
   );
 }
